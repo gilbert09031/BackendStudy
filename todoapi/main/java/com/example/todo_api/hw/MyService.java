5bdc7d7d66cc818095303e83e0ac3014419f9137
gilbert09031 <81512847+gilbert09031@users.noreply.github.com>
2025-04-09 22:21:56 +0900

    commit
    commit

---
+++ todoapi/main/java/com/example/todo_api/hw/MyService.java
@@ -0,0 +1,18 @@
+package com.example.todo_api.hw;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@RequiredArgsConstructor
+public class MyService {
+    private final MyRepository myRepository;
+
+    public void serviceMethod() {
+        System.out.println("service");
+        myRepository.repositoryMethod();
+    }
+}
